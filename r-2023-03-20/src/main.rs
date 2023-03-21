da9801cf4180a938a17305b6bac8ddd513df0f08
WhiteBlackGoose <wbg@angouri.org>
2023-03-21 13:16:00 +0600

    q research wip

--- r-2023-03-20/src/main.rs
+++ r-2023-03-20/src/main.rs
@@ -14,10 +14,23 @@ fn size_per_euro(area: f32, price: f32) -> f32 {
 
 /// Entry point
 fn main() {
-    let diam: f32 = 5.0;
-    let price: f32 = 10.0;
-    let area = area(diam);
-    println!("Area {area}");
-    let size_per_euro = size_per_euro(area, price);
-    println!("Size per euro {size_per_euro}");
+    // Task 1
+    // let diam: f32 = 5.0;
+    // let price: f32 = 10.0;
+    // let area = area(diam);
+    // println!("Area {area}");
+    // let size_per_euro = size_per_euro(area, price);
+    // println!("Size per euro {size_per_euro}");
+
+    // Task 2
+    let mut a = 1;
+    let mut b = 1;
+    let mut i = 0;
+    loop {
+        println!("{a}");
+        a = a + b;
+        (a, b) = (b, a);
+        i = i + 1;
+        if i >= 15 { break; }
+    }
 }
