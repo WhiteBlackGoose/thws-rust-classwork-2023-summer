a46fec6ba5c90301464b697c8a8cff730680b2f0
WhiteBlackGoose <wbg@angouri.org>
2023-04-13 12:16:57 +0200

    WIP

---
+++ r-2023-04-13/src/main.rs
@@ -0,0 +1,30 @@
+use std::{thread, time::Duration, sync::{Arc, Mutex}};
+
+fn main() {
+    let s: String = String::from("¡¡¡¡¡Hello");
+
+    let val = Arc::new(Mutex::new(s.clone()));
+    let arc1 = val.clone();
+    let t1 = thread::spawn(move || { 
+        for _ in 0..5 {
+            arc1.lock().unwrap().push('!');
+            thread::sleep(Duration::from_secs(1));
+        };
+    });
+
+    let arc2 = val.clone();
+    let t2 = thread::spawn(move || { 
+        for _ in 0..5 {
+            arc2.lock().unwrap().remove(0);
+            thread::sleep(Duration::from_secs(1));
+        };
+    });
+
+    for _ in 0..5 {
+        println!("{}", s);
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    t1.join().expect("ohno"); t2.join().expect("ohno2");
+    println!("{}", val.lock().unwrap());
+}
