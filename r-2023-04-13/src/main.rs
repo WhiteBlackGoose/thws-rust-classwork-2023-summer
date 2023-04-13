90ef20d94e46b025fbf4ad9d353791e06cec475c
WhiteBlackGoose <wbg@angouri.org>
2023-04-13 12:58:21 +0200

    WIP

--- r-2023-04-13/src/main.rs
+++ r-2023-04-13/src/main.rs
@@ -1,31 +1,34 @@
-use std::{thread, time::Duration, sync::{Arc, Mutex}};
+fn parse(inp: &str) -> Option<i32> {
+    match inp {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        "zero" => Some(0),
+        _ => None
+    }
+}
+
+fn get_num() -> Option<i32> {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).expect("");
+    parse(buf.as_str().trim())
+}
 
 fn main() {
-    let s: String = String::from("¡¡¡¡¡Hello");
-
-    let val = Arc::new(Mutex::new(s.clone()));
-
-    let arc1 = val.clone();
-    let t1 = thread::spawn(move || { 
-        for _ in 0..5 {
-            arc1.lock().unwrap().push('!');
-            thread::sleep(Duration::from_secs(1));
-        };
-    });
-
-    let arc2 = val.clone();
-    let t2 = thread::spawn(move || { 
-        for _ in 0..5 {
-            arc2.lock().unwrap().remove(0);
-            thread::sleep(Duration::from_secs(1));
-        };
-    });
-
-    for _ in 0..5 {
-        println!("{}", val.lock().unwrap());
-        thread::sleep(Duration::from_secs(1));
-    }
-
-    t1.join().expect("ohno"); t2.join().expect("ohno2");
-    println!("{}", val.lock().unwrap());
+    let a1 = get_num().expect("Bad input!");
+    let a2 = get_num().expect("Bad input!");
+    let a3 = get_num().expect("Bad input!");
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).expect("");
+    match buf.as_str().trim() {
+        "+" => println!("{}", a1 + a2 + a3),
+        "*" => println!("{}", a1 * a2 * a3),
+        _ => panic!("Ohno!")
+    };
 }
