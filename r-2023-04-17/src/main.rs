dcfb9d8ff03796247f68c72d4136ed211f043f01
WhiteBlackGoose <wbg@angouri.org>
2023-04-19 23:58:11 +0200

    exercise #4 done

--- r-2023-04-17/src/main.rs
+++ r-2023-04-17/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 struct Student<T> {
     id: T,
@@ -5,8 +7,23 @@ struct Student<T> {
     last_name: String,
 }
 
+impl<T: Display> Student<T> {
+    fn pretty_print(&self) {
+        println!("{} {} ({})", self.first_name, self.last_name, self.id);
+    }
+
+    fn pretty_print_censored(&self, forbidden: char) {
+        let id_as_str = format!("{}", self.id);
+        if id_as_str.contains(forbidden) {
+            return;
+        }
+        println!("{} {} ({})", self.first_name, self.last_name, id_as_str);
+    }
+}
 
 fn main() {
-    println!("{:#?}", Student { id: 555, first_name: String::from("Aaa"), last_name: String::from("Bbb") });
-    println!("{:#?}", Student { id: "Aaa", first_name: String::from("Aaa"), last_name: String::from("Bbb") });
+    Student { id: 555, first_name: String::from("Aaa"), last_name: String::from("Bbb") }.pretty_print();
+    Student { id: "Aaa", first_name: String::from("Aaa"), last_name: String::from("Bbb") }.pretty_print();
+    Student { id: "Aaa", first_name: String::from("Aaa"), last_name: String::from("Bbb") }.pretty_print_censored('A');
+    Student { id: "Aaa", first_name: String::from("Aaa"), last_name: String::from("Bbb") }.pretty_print_censored('B');
 }
