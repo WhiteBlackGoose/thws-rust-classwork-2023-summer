c1c8f499cd41039fc099aa9fda0b1b664ab6e67d
WhiteBlackGoose <wbg@angouri.org>
2023-04-24 16:45:57 +0200

    WIP

--- r-2023-04-24/src/main.rs
+++ r-2023-04-24/src/main.rs
@@ -1,7 +1,38 @@
-mod bmi_test;
-mod bmi;
+mod server {
+    fn compute_bmi(weight: f32, height: f32) -> f32 {
+        let height_m = height / 100.;
+        let bmi = weight / height_m.powi(2);
+        let rounded = (bmi * 10.0).round() / 10.0;
+        rounded
+    }
 
+    fn interpret(bmi: f32) -> String {
+        match bmi {
+            bmi if bmi < 15.0 => String::from("very severely underweight"),
+            bmi if bmi < 16.0 => String::from("severely underweight"),
+            bmi if bmi < 18.5 => String::from("underweight"),
+            bmi if bmi < 25.0 => String::from("normal weight"),
+            bmi if bmi < 30.0 => String::from("overweight"),
+            bmi if bmi < 35.0 => String::from("moderately obese"),
+            bmi if bmi < 40.0 => String::from("severely obese"),
+            _ => String::from("very severely obese")
+        }
+    }
+
+    pub async fn start() -> Result<(), reqwest::Error> {
+        loop {
+        }
+    }
+}
+
+mod client {
+    pub async fn request() {
+        
+    }
+}
+
+#[tokio::main]
 fn main() {
-    println!("{}", crate::bmi_test::bmi_test::validate("normal weight", 20.5));
-    println!("{}", crate::bmi_test::bmi_test::validate("overweight", 20.5));
+    println!("{}", bmi_test::validate("normal weight", 20.5));
+    println!("{}", bmi_test::validate("overweight", 20.5));
 }
