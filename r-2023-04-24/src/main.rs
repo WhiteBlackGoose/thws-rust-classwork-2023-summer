55b59a64c36bcff5c34127d0392c494c4af27635
WhiteBlackGoose <wbg@angouri.org>
2023-04-24 15:59:52 +0200

    sem 2023-04-24

---
+++ r-2023-04-24/src/main.rs
@@ -0,0 +1,33 @@
+mod bmi {
+    pub fn compute_bmi(weight: f32, height: f32) -> f32 {
+        let height_m = height / 100.;
+        let bmi = weight / height_m.powi(2);
+        let rounded = (bmi * 10.0).round() / 10.0;
+        rounded
+    }
+
+    pub fn interpret(bmi: f32) -> String {
+        match bmi {
+            bmi if bmi < 15.0 => String::from("very severely underweight"),
+            bmi if bmi < 16.0 => String::from("severely underweight"),
+            bmi if bmi < 18.5 => String::from("underweight"),
+            bmi if bmi < 25.0 => String::from("normal weight"),
+            bmi if bmi < 30.0 => String::from("overweight"),
+            bmi if bmi < 35.0 => String::from("moderately obese"),
+            bmi if bmi < 40.0 => String::from("severely obese"),
+            _ => String::from("very severely obese")
+        }
+    }
+}
+
+mod bmi_test {
+    pub fn validate(interpretation: &str, bmi: f32) -> bool {
+        let s: &str = &bmi::interpret(bmi);
+        s.eq(interpretation)
+    }
+}
+
+fn main() {
+    println!("{}", bmi_test::validate("normal weight", 20.5));
+    println!("{}", bmi_test::validate("overweight", 20.5));
+}
