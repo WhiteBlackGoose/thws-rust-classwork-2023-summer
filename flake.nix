3cdc2edd63644b3c38acf50d129aa80e269e29d5
WhiteBlackGoose <wbg@angouri.org>
2023-05-11 12:36:58 +0200

    toolchain changed

--- flake.nix
+++ flake.nix
@@ -1,20 +1,27 @@
+# Borrowed some stuff from https://ayats.org/blog/nix-rustup/
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay"; 
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, rust-overlay, ... }:
       let 
         systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ]; 
       in {
         devShells = nixpkgs.lib.genAttrs systems (system: 
         let 
-          pkgs = nixpkgs.legacyPackages.${system}; in
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [rust-overlay.overlays.default];
+          };
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+          in
         {
           default =
           pkgs.mkShell {
             buildInputs = with pkgs; [
-              cargo
-              rustc
-              rust-analyzer
+              toolchain
+              rust-analyzer-unwrapped
+              wasm-pack
               vscode-extensions.vadimcn.vscode-lldb
               openssl cmake zlib
               (
@@ -27,6 +34,7 @@
             VSCODE_CODELLDB = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}";
             OPENSSL_DIR="${pkgs.openssl.dev}";
             OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib";
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
           };
         });
     };
